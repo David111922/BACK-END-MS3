9e2e9b58c71638cd4e87b44eb86832d361f7583f
David111922 <salve62@yahoo.com>
2024-09-30 00:39:29 -0700

    added users table for login and migrated to pgAdmin

--- controller/user.js
+++ controller/user.js
@@ -1,15 +1,27 @@
-// const router = require('express').Router()
-// const db = require("../models")
+const router = require('express').Router()
+const db = require("../models")
 
-// router.post('/', async (req, res) => {
-//     const user = await User.create(req.body)
-//     res.json(user)
-// })
+router.post('/', async (req, res) => {
+    try{
+    //! snippet below used to create new user
+    const user = await db. User.create(req.body)
+    // 201 indicates successfully processed .201 for post
+    res.status(201).json(user);
+} catch (error) {
+    console.error("Error creating user:", error);
+    res.status(500).json({ error: "Failed to create user" });
+}
+});
 
 
-// router.get('/', async (req, res) => {
-//     const users = await User.findAll()
-//     res.json(users)
-// })
-
-// module.exports = router
+router.get('/', async (req, res) => {
+    try{
+    // !snippet below used to retrieve all users from db
+    const users = await db.User.findAll()
+    res.status(200).json(users);
+    } catch (error) {
+        console.error("Error retrieving users:", error);
+        res.status(500).json({ error: "Failed to retrieve users" });
+    }
+});
+module.exports = router
